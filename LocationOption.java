32a0d46efc599f09270b6dd449ada3c0a20b85ff
mdkul22 <mdkul22@gmail.com>
2018-06-13 16:40:10 +0530

    adding window for location configuration

--- LocationOption.java
+++ LocationOption.java
@@ -2,6 +2,8 @@ package com.cisco.rakeye;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class LocationOption extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class LocationOption extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_option);
+        // building options
+        Spinner bspinner = findViewById(R.id.spinner2);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+                R.array.building_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        bspinner.setAdapter(adapter1);
+        // floor options
+        Spinner fspinner = findViewById(R.id.spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.floor_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        fspinner.setAdapter(adapter2);
     }
+
 }
