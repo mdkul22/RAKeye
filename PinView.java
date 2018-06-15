286bb5485d12bdf8c72c0ee40042732ea57cf8a7
mdkul22 <mdkul22@gmail.com>
2018-06-15 13:51:56 +0530

    Pin set up working properly, will start mqtt launch using paho client on next activity

---
+++ PinView.java
@@ -0,0 +1,61 @@
+package com.cisco.rakeye;
+
+import android.content.Context;
+import android.graphics.*;
+import android.util.AttributeSet;
+
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
+
+public class PinView extends SubsamplingScaleImageView {
+
+    private final Paint paint = new Paint();
+    private final PointF vPin = new PointF();
+    private PointF sPin;
+    private Bitmap pin;
+    Context context;
+    public PinView(Context context) {
+        this(context, null);
+        this.context = context;
+    }
+
+    public PinView(Context context, AttributeSet attr) {
+        super(context, attr);
+        initialise();
+    }
+
+    public void setPin(PointF sPin) {
+        this.sPin = sPin;
+        initialise();
+        invalidate();
+    }
+
+    private void initialise() {
+        float density = getResources().getDisplayMetrics().densityDpi;
+        pin = BitmapFactory.decodeResource(this.getResources(), R.drawable.green_dot);
+        float w = (density/21000f) * pin.getWidth();
+        float h = (density/21000f) * pin.getHeight();
+        pin = Bitmap.createScaledBitmap(pin, (int)w, (int)h, true);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        // Don't draw pin before image is ready so it doesn't move around during setup.
+        if (!isReady()) {
+            return;
+        }
+
+        paint.setAntiAlias(true);
+
+        if (sPin != null && pin != null) {
+            sourceToViewCoord(sPin, vPin);
+            float vX = vPin.x - (pin.getWidth()/2);
+            float vY = vPin.y - pin.getHeight();
+            canvas.drawBitmap(pin, vX, vY, paint);
+        }
+
+    }
+
+}
--- PopPlan.java
+++ PopPlan.java
@@ -1,16 +1,24 @@
 package com.cisco.rakeye;
 
+import android.app.Activity;
+import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
+
 public class PopPlan extends AppCompatActivity {
     private static final String TAG="PopPlan";
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop_plan);
@@ -19,53 +27,223 @@ public class PopPlan extends AppCompatActivity {
         String floor = ((MyApplication) getApplication()).getFloor();
         if(bld.equals("BGL12")) {
             if(floor.equals("0")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl120));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
                 Log.d(TAG, "Entered bgl 12 ground");
             }
             else if(floor.equals("1")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl121));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
 
             else if(floor.equals("2")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl122));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
 
             else if(floor.equals("3")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl123));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
 
             else if(floor.equals("4")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl124));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
             }
         if(bld.equals("BGL11")) {
             if(floor.equals("0")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl110));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
             else if(floor.equals("1")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl111));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
 
             else if(floor.equals("2")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl112));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
 
             else if(floor.equals("3")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl113));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
 
             else if(floor.equals("4")) {
-                SubsamplingScaleImageView imageView = findViewById(R.id.imageView);
+                final PinView imageView = (PinView)findViewById(R.id.imageView);
                 imageView.setImage(ImageSource.resource(R.drawable.bgl114));
+                final GestureDetector gestureDetector = new GestureDetector(this, new GestureDetector.SimpleOnGestureListener() {
+                    @Override
+                    public boolean onSingleTapConfirmed(MotionEvent e) {
+                        if (imageView.isReady()) {
+                            PointF sCoord = imageView.viewToSourceCoord(e.getX(), e.getY());
+                            imageView.setPin(sCoord);
+                            // ...
+                        }
+                        return true;
+                    }
+                });
+                imageView.setOnTouchListener(new View.OnTouchListener() {
+                    @Override
+                    public boolean onTouch(View view, MotionEvent motionEvent) {
+                        return gestureDetector.onTouchEvent(motionEvent);
+                    }
+                });
             }
         }
     }
